2836a76f6db9323517da214e5cac100fb6ad3529
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-02 13:06:01 +0530

    added json URL and saveData function to post data to JSON

--- src/app/registerservice.service.ts
+++ src/app/registerservice.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Employee } from './model/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterserviceService {
 
-  constructor() { }
+  private URL: string = "http://localhost:3000/employee";
+
+  constructor(private httpClient: HttpClient) { }
+
+  saveData(employeeData: Employee) {
+    this.httpClient.post(this.URL,employeeData);
+  }
 }
