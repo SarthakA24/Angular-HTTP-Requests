d31e17b354ef04eb2e369ae285fb1b19352b5a68
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-11-02 13:01:14 +0530

    added an object for Employee

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../model/employee';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  employeeDetail: Employee = {};
   constructor() { }
 
   ngOnInit(): void {
